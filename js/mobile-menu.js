b202c2697813a2bbdd079f7811f2da9549c2a57a
VVolDan <vdan2019@gmail.com>
2022-01-02 11:03:37 +0200

    адаптивна верстка

---
+++ js/mobile-menu.js
@@ -0,0 +1,28 @@
+(() => {
+  const mobileMenu = document.querySelector('[data-menu-container]');
+  const openMenuBtn = document.querySelector('[data-mobile-btn]');
+  const closeMenuBtn = document.querySelector('[data-mobile-btn]');
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
